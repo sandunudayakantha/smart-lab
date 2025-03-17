a69815735c3c1f9a7ea0712d06b3d529690e2c22
sandunudayakantha <sndnudayakantha@gmail.com>
2025-03-17 22:07:26 +0530

    edit

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Create Template</button>
+    </div>
+  )
+}
+
+export default Home
